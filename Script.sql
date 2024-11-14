a2809485681c3aaa4dc195c48a122d5bf04c5997
Daniela2 <alo2@gmail.com>
2024-11-14 14:54:42 -0500

    Se actualiza el archivo

--- Script.sql
+++ Script.sql
@@ -1,3 +1,13 @@
-SELECT idEstudiante, identificacion, nombres, apellidos, edad, email, activo
+SELECT DISTINCT idEstudiante, identificacion, nombres, apellidos, edad, email, activo
 FROM Estudiantes 
 WHERE activo = "S" AND email LIKE "%@gmail.com";
+
+SELECT  idEstudiante, identificacion, nombres, apellidos, edad, email, activo
+FROM Estudiantes 
+WHERE activo = "N" AND edad >18 and Estudiantes.identificacion LIKE"1%";
+
+SELECT DISTINCT a.idEstudiante, a.identificacion, a.nombres, a.apellidos, a.edad, a.email, a.activo
+FROM Estudiantes a
+JOIN MateriasEstudiantes c ON a.idEstudiante = c.idEstudiante
+JOIN Materias b ON b.idMateria = c.idMateria
+WHERE a.activo = "S" AND b.activo = "S";
